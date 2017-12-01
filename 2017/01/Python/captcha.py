4c1af2627caea2e50c14b24d3b9e048dd9fefc8b
Rahul Butani <rr.butani@gmail.com>
2017-12-01 02:01:47 -0600

    2017: Housekeeping

---
+++ 2017/01/Python/captcha.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+a = input()
+
+print("p1: ", sum( [ int(n) for i, n in enumerate(a) if ( a[i] == a[ (i + 1)             % len(a) ] ) ] ))
+
+print("p2: ", sum( [ int(n) for i, n in enumerate(a) if ( a[i] == a[ (i + int(len(a)/2)) % len(a) ] ) ] ))
+
+##############################
+# Author: Rahul Butani       #
+# Date:   December 1st, 2017 #
+##############################
