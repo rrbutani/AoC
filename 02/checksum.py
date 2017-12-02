c5a06c5102d82bd6f4bb221813ad48cf595b7d5c
Rahul Butani <rr.butani@gmail.com>
2017-12-02 04:55:46 -0600

    Cleaned up a little

--- 02/checksum.py
+++ 02/checksum.py
@@ -22,53 +22,74 @@
 
 import itertools
 
-sum = 0
+# with open("in.txt") ad f:
 
-while(True):
-    # the_string = input()
-    time.sleep(0.2)
+# print([ [ max(t) // min(t) for t in itertools.combinations(l, 2) if not max(t) % min(t) ] for l in [ [int(x) for x in line.split('\t')] for line in open("in.txt") ] ])
 
-    lo = input()
+# [   ] 
+print(sum( [max(l) -  min(l) for l in [ [int(x) for x in line.split('\t')] for line in open("in.txt")] ] ))
 
-    # print(lo)
-
-    if(lo == ''):
-        print("YO")
-        break
-
-    # l = map(int, lo.split('\t'))#lo.split('\t')
-
-    l = [ int(s) for s in lo.split('\t') ]
-
-    # print(itertools.combinations(l, 2))
-
-    for subset in itertools.combinations(l, 2):
-        # print("ay", subset[0], subset[1], subset[0] / subset[1])
-        if (float(max(subset)) / min(subset)) % 1 == 0.0:
-            sum += float(max(subset)) / min(subset)
-            # print("ay", subset[0], subset[1], float(max(subset)) / min(subset))
-            break
-        # print(subset[0], subset[1])
-
-    # print(l)
-
-    # print(l, "max: ", max(l), "min: ", min(l))
-
-    # print("max is ", max(l), " min is ", min(l))
-
-    # sum += int(max(l)) - int(min(l))
+print(sum( [max(t) // min(t) for l in [ [int(x) for x in line.split('\t')] for line in open("in.txt")] for t in itertools.combinations(l, 2) if not max(t) % min(t) ]))
 
 
-    # print("NEW ", sum)
-# print(min(l))
-# print(max(l))
 
-print(sum)
-
-# print(name)
-# print(age)
+# sum('a')
 
 
-# 5 9 2 8
-# 9 4 7 3
-# 3 8 6 5
+# print(sum( [max(t) // min(t) for t in [itertools.combinations(l, 2) for l in [ [int(x) for x in line.split('\t')] for line in open("in.txt")] ] if not max(t) % min(t)] ))
+# [ for t in [itertools.combinations(l, 2) for l in [ [int(x) for x in line.split('\t')] for line in open("in.txt")] ] ]
+
+# max(line.split('\t')) - min(line.split('\t'))
+
+# sum = 0
+
+# while(True):
+#     # the_string = input()
+#     time.sleep(0.2)
+
+#     lo = input()
+
+#     # print(lo)
+
+#     if(lo == ''):
+#         print("YO")
+#         break
+
+#     # l = map(int, lo.split('\t'))#lo.split('\t')
+
+#     l = [ int(s) for s in lo.split('\t') ]
+
+#     # print(itertools.combinations(l, 2))
+
+#     for subset in itertools.combinations(l, 2):
+#         # print("ay", subset[0], subset[1], subset[0] / subset[1])
+#         if (float(max(subset)) / min(subset)) % 1 == 0.0:
+#             sum += float(max(subset)) / min(subset)
+#             # print("ay", subset[0], subset[1], float(max(subset)) / min(subset))
+#             break
+#         # print(subset[0], subset[1])
+    
+#     l/2 * l 
+
+#     # print(l)
+
+#     # print(l, "max: ", max(l), "min: ", min(l))
+
+#     # print("max is ", max(l), " min is ", min(l))
+
+#     # sum += int(max(l)) - int(min(l))
+
+
+#     # print("NEW ", sum)
+# # print(min(l))
+# # print(max(l))
+
+# print(sum)
+
+# # print(name)
+# # print(age)
+
+
+# # 5 9 2 8
+# # 9 4 7 3
+# # 3 8 6 5
