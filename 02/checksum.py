6d2161ec79b844b3f27bc6a9ffc38c4278b50961
Rahul Butani <rr.butani@gmail.com>
2017-12-02 00:31:46 -0600

    This works but is ugly

---
+++ 02/checksum.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+# import sys
+
+
+# sum = 0
+
+# inp = sys.stdin.read()
+
+# print(raw_input())
+
+# while(sys.stdin.read()):
+#     l = raw_input().split()
+#     print(l)
+#     sum += max(l) - min(l)
+#     inp = sys.stdin.read()
+
+#     print(sum)
+
+# print(sum)
+# 
+#
+
+import itertools
+
+sum = 0
+
+while(True):
+    # the_string = input()
+    time.sleep(0.2)
+
+    lo = input()
+
+    # print(lo)
+
+    if(lo == ''):
+        print("YO")
+        break
+
+    # l = map(int, lo.split('\t'))#lo.split('\t')
+
+    l = [ int(s) for s in lo.split('\t') ]
+
+    # print(itertools.combinations(l, 2))
+
+    for subset in itertools.combinations(l, 2):
+        # print("ay", subset[0], subset[1], subset[0] / subset[1])
+        if (float(max(subset)) / min(subset)) % 1 == 0.0:
+            sum += float(max(subset)) / min(subset)
+            # print("ay", subset[0], subset[1], float(max(subset)) / min(subset))
+            break
+        # print(subset[0], subset[1])
+
+    # print(l)
+
+    # print(l, "max: ", max(l), "min: ", min(l))
+
+    # print("max is ", max(l), " min is ", min(l))
+
+    # sum += int(max(l)) - int(min(l))
+
+
+    # print("NEW ", sum)
+# print(min(l))
+# print(max(l))
+
+print(sum)
+
+# print(name)
+# print(age)
+
+
+# 5 9 2 8
+# 9 4 7 3
+# 3 8 6 5
