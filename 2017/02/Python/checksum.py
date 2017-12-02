2f24b6541995c80303f01eb12235a097f6600d09
Rahul Butani <rr.butani@gmail.com>
2017-12-02 10:11:10 -0600

    2017: Day 2, for now

---
+++ 2017/02/Python/checksum.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from itertools import combinations as cmbs
+
+print(sum( [max(l) -  min(l) for l in [ [int(x) for x in line.split('\t')] for line in open("input")] ] ))
+
+print(sum( [max(t) // min(t) for l in [ [int(x) for x in line.split('\t')] for line in open("input")] for t in cmbs(l, 2) if not max(t) % min(t) ]))
+
+##############################
+# Author: Rahul Butani       #
+# Date:   December 2nd, 2017 #
+##############################
