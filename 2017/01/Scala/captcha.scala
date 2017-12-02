df4fa4c1e3631d5819acafafb51478705a0c4b25
Rahul Butani <rr.butani@gmail.com>
2017-12-02 10:10:18 -0600

    2017: Housekeeping

--- 2017/01/Scala/captcha.scala
+++ 2017/01/Scala/captcha.scala
@@ -1,9 +1,4 @@
-
 val input = scala.io.StdIn.readLine().split("").map(_.toInt)
-
-// for ( i <- (input zip (input.slice(1) ++ input.slice(0, 1)).drop(1)) ) println(i)
-
-// for (i <- (input :+ input.head).sliding(2).toList) println(i)
 
 println(s"P1: ${(input :+ input.head).sliding(2).toList.foldLeft(0)((s, t) => (if (t(0) == t(1)) t(0) + s else s))}") 
 println(s"P2: ${(input zip (input.drop(input.length/2) ++ input.take(input.length/2))).foldLeft(0)((s, t) => if (t._1 == t._2) t._1 + s else s)}")
