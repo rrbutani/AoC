9d136e98fafaf9a54f7d8285340c316f52b8fa64
Rahul Butani <rr.butani@gmail.com>
2017-12-02 10:11:10 -0600

    Day 2, for now

---
+++ 02/Haskell/checksum.hs
@@ -0,0 +1,13 @@
+import Data.List
+
+main = do
+    g <- readFile("input")
+    let l = [ [ read w :: Int | w <- h] | l <- lines g, let h = words l]
+    
+    print( sum( [ maximum x - minimum x | x <- l ] ) )
+    print( sum (concat [ [ maximum x `div` minimum x | x <- r, maximum x `mod` minimum x == 0 ] | r <- [ [ x:y:[] | (x:xs) <- tails ls, y:_ <- tails xs ] | ls <- l]]))
+
+--------------------------------
+-- Author: Rahul Butani       --
+-- Date:   December 2nd, 2017 --
+--------------------------------
