7516688bed13a53848c297145e06fe98f31224ee
Rahul Butani <rr.butani@gmail.com>
2017-12-06 10:37:48 -0600

    2017: Cleaned up Day 6 Python

--- 2017/06/Python/mancala.py
+++ 2017/06/Python/mancala.py
@@ -1,62 +1,21 @@
 #!/usr/bin/env python3
 
-l = [int(i) for x in open("input") for i in x.split()]
-# l = [0, 2, 7, 0]
-s = set()#set([tuple(l)])
+mbs = [int(i) for x in open("input") for i in x.split()]
 
-# print(''.join(str(i) + ' ' for i in l))
+configs = {}
+rcs = 0
 
-count = 0
+while tuple(mbs) not in configs:
+    configs[tuple(mbs)] = rcs
 
-while((''.join(str(i) + ' ' for i in l) in s) == False):
-    s.add(''.join(str(i) + ' ' for i in l))
+    indx = mbs.index(max(mbs))
+    blks = mbs[indx]
+    mbs[indx] = 0
 
-    idx = l.index(max(l))
-    cnt = l[idx]
-    l[idx] = 0
+    for i in range(blks):
+        mbs[(indx + i + 1) % len(mbs)] += 1
 
-    while(cnt > 0):
-        idx += 1
-        cnt -= 1
-        l[idx % len(l)] += 1
+    rcs += 1
 
-    print(l)
-
-    count += 1
-
-print(l)
-print(count)
-
-matches = 0
-count = 0
-
-ll = [int(i) for x in open("input") for i in x.split()]
-# ll = [0, 2, 7, 0]
-s = set()#set([tuple(l)])
-
-while((''.join(str(i) + ' ' for i in ll) in s) == False):
-    s.add(''.join(str(i) + ' ' for i in ll))
-
-    idx = ll.index(max(ll))
-    cnt = ll[idx]
-    ll[idx] = 0
-
-    while(cnt > 0):
-        idx += 1
-        cnt -= 1
-        ll[idx % len(ll)] += 1
-
-    print(ll, l)
-    # 
-    if l == ll:
-        print("MATCH")
-
-    if l != ll and matches == 1:
-        print("INC")
-        count += 1
-
-    if l == ll and matches == 0:
-        print("SET") 
-        matches = 1
-
-print(count+1)
+print(f"P1: {rcs}")
+print(f"P2: {rcs - configs[tuple(mbs)]}")
