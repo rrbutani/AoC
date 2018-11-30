9a06e8aafa7df13583e3543e70933c512ccda0b2
Rahul Butani <rr.butani@gmail.com>
2018-11-30 10:04:21 -0600

    checkpoint: compiles

--- common/rust/aoc/src/lib.rs
+++ common/rust/aoc/src/lib.rs
@@ -2,22 +2,29 @@
 #![feature(range_contains)]
 
 extern crate clap;
+extern crate http;
 extern crate reqwest;
+extern crate select;
 
 use std::path::{Component, Path, PathBuf};
 use std::io::prelude::*;
 use std::fs::File;
 
-use tap::TapOps;
 use clap::{Arg, ArgGroup, App};
+use tap::TapOps;
 
 use crate::AdventOfCodeClient::{AocClient, AocError, Part};
 
-const YEAR: u16 = 2017;
+const YEAR: u16 = 2018;
 
 mod AdventOfCodeClient {
 
-    #[derive(PartialEq)]
+    use http::StatusCode;
+    use reqwest::{Client, Error as RequestError, header, Url};
+    use select::document::Document;
+    use select::predicate::{Predicate, Attr, Class, Name};
+
+    #[derive(Debug, PartialEq)]
     pub struct AocClient {
         day: u8,
         year: u16,
@@ -25,10 +32,31 @@ mod AdventOfCodeClient {
     }
 
     #[derive(Debug)]
+    pub enum ErrDirection {
+        TooHigh,
+        TooLow,
+        Unknown
+    }
+
+    impl ErrDirection {
+        pub fn to_string(&self) -> &'static str {
+            match self {
+                ErrDirection::TooHigh => "The answer was given to be too high.",
+                ErrDirection::TooLow => "The answer was given to be too low.",
+                ErrDirection::Unknown => "No indication about the answer was given.",
+            }
+        }
+    }
+
+    #[derive(Debug)]
     pub enum AocError {
-        UnableToSubmit,
-        WrongAnswer,
-        OtherError(String),
+        AuthError(RequestError),
+        NotFound(RequestError),
+        InvalidToken(String),
+        WrongAnswer(ErrDirection),
+        Timeout(u8),
+        RequestError(RequestError),
+        UnknownError(String),
     }
 
     pub enum Part {
@@ -36,7 +64,56 @@ mod AdventOfCodeClient {
         Two,
     }
 
+    impl Part {
+        pub fn to_string(&self) -> &'static str {
+            match self {
+                Part::One => "one",
+                Part::Two => "two",
+            }
+        }
+    }
+
     pub type AocResult = Result<String, AocError>;
+
+    fn get_client(token: &String) -> Result<Client, AocError> {
+        let mut headers = header::HeaderMap::new();
+        headers
+            .insert(header::COOKIE, 
+                header::HeaderValue::from_str(&token)
+                    .map_err(|e| AocError::InvalidToken(e.to_string()))?);
+
+        reqwest::Client::builder()
+            .default_headers(headers)
+            .gzip(true)
+            .build()
+            .map_err(|e| AocError::RequestError(e))
+    }
+
+    // static base: Box<Fn(u16, u8) -> String> = Box::new(|y, d| format!("https://adventofcode.com/{}/day/{}", y, d));
+
+    fn base(y: u16, d: u8) -> String {
+        format!("https://adventofcode.com/{}/day/{}", y, d)
+    }
+
+    fn get_input_url(year: u16, day: u8) -> String {
+        format!("{}/input", base(year, day))
+    }
+
+    fn get_output_url(year: u16, day: u8) -> String {
+        format!("{}/answer", base(year, day))
+    }
+
+    fn err_mapper(err: RequestError) -> AocError {
+        if let Some(status) = err.status() {
+            match status {
+                StatusCode::BAD_REQUEST => AocError::AuthError(err),
+                StatusCode::NOT_FOUND => AocError::NotFound(err),
+                _ => AocError::RequestError(err),
+            }
+        } else {
+            AocError::RequestError(err)
+        }
+    }
 
     impl AocClient {
         pub fn new(year: u16, day: u8, token: String) -> Result<Self, String> {
@@ -44,45 +121,83 @@ mod AdventOfCodeClient {
                 return Err(format!("Invalid day: {}", day));
             }
 
-            unimplemented!()
+            Ok(AocClient { year, day, token })
         }
 
-        pub fn get_input(self) -> AocResult {
-            get_input(self.year, self.day, self.token)
+        pub fn get_input(&self) -> AocResult {
+            get_input(self.year, self.day, &self.token)
         }
 
-        pub fn submit_answer(self, answer: String) -> AocResult {
-            submit_answer(self.year, self.day, self.token, answer)
+        pub fn submit_answer(&self, part: &Part, answer: &String) -> AocResult {
+            submit_answer(self.year, self.day, &self.token, part, answer)
         }
 
-        pub(crate) fn get_token(self) -> String {
-            self.token
+        pub(crate) fn get_token(&self) -> &String {
+            &self.token
         }
     }
 
-    pub fn get_input(year: u16, day: u8, token: String) -> AocResult {
-        unimplemented!()
+    pub fn get_input(year: u16, day: u8, token: &String) -> AocResult {
+        let client = get_client(token)?;
+        let url = Url::parse(&get_input_url(year, day))
+            .map_err(|e| AocError::UnknownError(e.to_string()))?;
+
+        let mut resp = client
+            .get(url)
+            .send()
+            .map_err(err_mapper)?
+            .error_for_status()
+            .map_err(err_mapper)?;
+
+        let body = resp.text().map_err(|e| AocError::RequestError(e))?;
+
+        match resp.status() {
+            StatusCode::OK => Ok(body),
+            _ => Err(AocError::UnknownError(body)),
+        }
     }
 
-    pub fn submit_answer(year: u16, day: u8, token: String, answer: String) -> AocResult {
-        unimplemented!()
+    pub fn submit_answer(year: u16, day: u8, token: &String, part: &Part, answer: &String) -> AocResult {
+        let client = get_client(token)?;
+        let url = Url::parse(&get_output_url(year, day))
+            .map_err(|e| AocError::UnknownError(e.to_string()))?;
+
+        let body = format!("level={}&answer={}",
+            match part { Part::One => 1, Part::Two => 2 },
+            answer);
+
+        let mut resp = client
+            .post(url)
+            .body(body)
+            .send()
+            .map_err(err_mapper)?
+            .error_for_status()
+            .map_err(err_mapper)?;
+
+        let body = resp.text().map_err(|e| AocError::RequestError(e))?;
+
+        // let doc = Document::from(body.as_str());
+        // doc.find(predicate)
+        Ok(body)
     }
 }
 
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 enum InputSource {
     File(String),
     Stdin,
     Web,
 }
 
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 enum OutputSink {
     StdOut,
     Web(AocClient)
 }
 
+#[derive(Debug)]
 struct Config {
+    year: u16,
     day: u8,
     input: InputSource,
     output: OutputSink,
@@ -95,13 +210,13 @@ macro_rules! cargo_env {
 }
 
 fn get_cached_file_path(day: u8, tok: Option<&str>) -> PathBuf {
-    let mut file_path = PathBuf::new();
+    let file_path = PathBuf::new();
 
     // Though we will never create an input file that doesn't have the token
     // specified (we only create this file when we grab the input from the
     // web which we can only do if we have a token), the user may create such a
     // file which is why we account for this possibility.
-    let file_name = format!("{}{}.input", day, tok.map_or("", |t| &format!("-{}", t)));
+    let file_name = format!("{}{}.input", day, tok.map_or(String::from(""), |t| format!("-{}", t)));
 
     file_path
         .tap(|f| f.push(Component::ParentDir))
@@ -111,14 +226,22 @@ fn get_cached_file_path(day: u8, tok: Option<&str>) -> PathBuf {
 
 impl Config {
     pub fn get_config(day: u8) -> Self {
-        Self::get_config_internal(day, None)
+        Self::get_config_internal(YEAR, day, None)
+    }
+
+    pub fn get_config_with_year(year: u16, day: u8) -> Self {
+        Self::get_config_internal(year, day, None)
     }
 
     pub fn get_config_with_token(day: u8, tok: &str) -> Self {
-        Self::get_config_internal(day, Some(tok))
+        Self::get_config_internal(YEAR, day, Some(tok))
     }
 
-    fn get_config_internal(day: u8, tok: Option<&str>) -> Self {
+    pub fn get_config_with_year_and_token(year: u16, day: u8, tok: &str) -> Self {
+        Self::get_config_internal(year, day, Some(tok))
+    }
+
+    fn get_config_internal(year: u16, day: u8, tok: Option<&str>) -> Self {
 
         // Args:
         //  - input: [ stdin | input file ]
@@ -211,7 +334,7 @@ impl Config {
 
         // Now check if the token (if we have one) is valid:
         let output = if let Some(token) = token {
-            OutputSink::Web(AocClient::new(YEAR, day, token).unwrap())
+            OutputSink::Web(AocClient::new(year, day, token).unwrap())
         } else {
             // If we don't have a valid token, fall back to printing out to stdout:
             eprint!("Warning: Printing results to stdout");
@@ -245,7 +368,7 @@ impl Config {
                 // this if we're configured to submit outputs too (if we're not and
                 // still running at this point it means we don't have tokens), so
                 // let's check that:
-                if let OutputSink::Web(web) = output {
+                if let OutputSink::Web(_) = output {
                     InputSource::Web
                 } else {
                     // If we have no way to take input, we must error!
@@ -254,25 +377,27 @@ impl Config {
             }
         };
 
-        Config { day, input, output }
+        Config { year, day, input, output }
     }
 
-    fn assert_config(self, inp: InputSource, out: OutputSink) -> bool {
+    pub fn assert_config(self, inp: InputSource, out: OutputSink) -> bool {
         self.input == inp && self.output == out
     }
 }
 
-struct AdventOfCode {
+#[derive(Debug)]
+pub struct AdventOfCode {
     config: Config,
     input: Option<String>
 }
 
-enum Error {
+#[derive(Debug)]
+pub enum Error {
     CannotSubmitAutomatically,
     AutoSubmitError(AocError),
 }
 
-type Result = std::result::Result<Option<String>, Error>;
+type Result = std::result::Result<String, Error>;
 
 impl AdventOfCode {
 
@@ -290,32 +415,35 @@ impl AdventOfCode {
         }
     }
 
-    pub fn get_input(&mut self) -> &String {
-        if let Some(input) = self.input {
-            &input
+    pub fn get_input(&mut self) -> String {
+        if let Some(input) = &self.input {
+            input.clone()
         } else {
             use self::InputSource::*;
-            match self.config.input {
+            match &self.config.input {
                 File(f) => {
                     let mut file = std::fs::File::open(f).expect(&format!("Unable to open `{}`.", f));
-                    let input = String::new();
+                    let mut input = String::new();
 
                     file.read_to_string(&mut input).expect(&format!("Unable to read `{}`.", f));
 
+                    let out = input.clone();
                     self.input = Some(input);
-                    &input
+                    out
                 },
                 Stdin => {
-                    let input = String::new();
-                    let mut handle = std::io::stdin().lock();
+                    let mut input = String::new();
+                    let stdin = std::io::stdin();
+                    let mut handle = stdin.lock();
 
                     handle.read_to_string(&mut input).expect("Unable to read from stdin");
 
+                    let out = input.clone();
                     self.input = Some(input);
-                    &input
+                    out
                 },
                 Web => {
-                    let aoc = match self.config.output {
+                    let aoc = match &self.config.output {
                         OutputSink::Web(aoc) => aoc,
                         _ => panic!("It shouldn't be possible to make an AdventOfCode struct\
                             that's configured to grab input from the web but *not* configured\
@@ -329,43 +457,54 @@ impl AdventOfCode {
                     // to cache the input to be nice to the Advent of Code
                     // servers:
                     let f = get_cached_file_path(self.config.day, Some(&aoc.get_token()));
-                    std::fs::write(f, input).expect(&format!("Couldn't write to file `{:?}`.", f));
+                    let fname = f.clone();
+                    let fname = fname.to_str().unwrap_or("<some file>");
+                    std::fs::write(f, &input).expect(&format!("Couldn't write to file `{}`.", fname));
 
+                    let out = input.clone();
                     self.input = Some(input);
-                    &input
+                    out
                 },
             }
         }
     }
 
-    fn submit<T: Into<String>>(&self, part: Part, answer: T) -> Result {
+    fn submit<T: Into<String>>(&mut self, part: &Part, answer: T) -> Result {
         use self::OutputSink::*;
-        match self.config.output {
+        match &self.config.output {
             StdOut => {
                 println!("{}", answer.into());
                 Err(Error::CannotSubmitAutomatically)
             },
             Web(aoc) => {
-
+                aoc.submit_answer(part, &answer.into()).map_err(|e| Error::AutoSubmitError(e))
             },
         }
     }
 
-    fn submit_with_feedback<T: Into<String>>(&self, part: Part, answer: T) -> Result {
+    fn submit_with_feedback<T: Into<String>>(&mut self, part: &Part, answer: T) -> Result {
         let res = self.submit(part, answer);
 
         use self::Error::*;
+        use self::AocError::*;
         match res {
-            Ok(message) => {
-
+            Ok(ref message) => {
+                println!("Success for part {}! Got: {}", part.to_string(), message);
             },
-            Err(err) => match err {
+            Err(ref err) => match err {
                 CannotSubmitAutomatically => {
                     eprintln!("Not configured to submit automatically.");
-                    eprintln!("Please go to `https://adventofcode.com/{}/day/{}` to submit!", YEAR, self.config.day);
+                    eprintln!("Please go to `https://adventofcode.com/{}/day/{}` to submit!", self.config.year, self.config.day);
                 },
                 AutoSubmitError(err) => match err {
-
+                    AuthError(err) => eprintln!("Authentication failed; check your token? Got: {}", err.to_string()),
+                    NotFound(err) => eprintln!("Got a 404; maybe it's too early? We're trying to submit Part {} of \
+                        Day {}, {}. Got: {}", part.to_string(), self.config.day, self.config.year, err.to_string()),
+                    InvalidToken(message) => eprintln!("Invalid token. Got: {}", message),
+                    WrongAnswer(dir) => eprintln!("Wrong answer! {}", dir.to_string()),
+                    Timeout(timeout) => eprintln!("Slow down! Hit a timeout: {}", timeout),
+                    RequestError(err) => eprintln!("Request Error: {}", err.to_string()),
+                    UnknownError(message) => eprintln!("Unknown Error: {}", message),
                 }
             }
         };
@@ -373,20 +512,17 @@ impl AdventOfCode {
         res
     }
 
-    pub fn submit_p1<T: Into<String>>(&self, answer: T) -> Result {
-        self.submit_with_feedback(Part::One, answer)
+    pub fn submit_p1<T: Into<String>>(&mut self, answer: T) -> Result {
+        self.submit_with_feedback(&Part::One, answer)
     }
 
-    pub fn submit_p2<T: Into<String>>(&self, answer: T) -> Result {
-        self.submit_with_feedback(Part::Two, answer)
+    pub fn submit_p2<T: Into<String>>(&mut self, answer: T) -> Result {
+        self.submit_with_feedback(&Part::Two, answer)
     }
 }
 
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+
 }
