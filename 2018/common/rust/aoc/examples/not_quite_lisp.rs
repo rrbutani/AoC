c2317196e6181edd98a04628cb4eff26d48f417f
Rahul Butani <rr.butani@gmail.com>
2018-11-30 14:04:23 -0600

    2018: polish and such

--- 2018/common/rust/aoc/examples/not_quite_lisp.rs
+++ 2018/common/rust/aoc/examples/not_quite_lisp.rs
@@ -9,4 +9,13 @@ fn main() {
     let level: i64 = input.chars().map(|c| match c { '(' => 1, ')' => -1, _ => 0 }).sum();
 
     aoc.submit_p1(level);
+
+    let mut sum: i64 = 0;
+    let neg: usize = input.chars()
+        .map(|c| { sum += match c { '(' => 1, ')' => -1, _ => 0 }; sum })
+        .position(|e| e < 0)
+        .unwrap() + 1;
+
+
+    aoc.submit_p2(neg);
 }
