08bd5b7e1550abaf418186f858c778943329b6e6
Rahul Butani <rr.butani@gmail.com>
2018-12-01 00:32:29 -0600

    cleaned!

--- 01/Rust/time_drift.rs
+++ 01/Rust/time_drift.rs
@@ -8,12 +8,9 @@ fn main() {
     let mut aoc = AdventOfCode::new_with_year(2018, 01);
     let input: String = aoc.get_input();
 
-    // println!("{}", input);
-
     let p1: i32 = input.lines().map(|f| f.parse::<i32>().unwrap()).sum();
 
-    // println!("{}", p1);
-    // aoc.submit_p1(p1);
+    aoc.submit_p1(p1);
 
     let mut hm: HashSet<i64> = HashSet::new();
     let mut current_freq: i64 = 0;
@@ -22,14 +19,9 @@ fn main() {
         .cycle()
         .map(|f| f.parse::<i64>().unwrap())
         .map(|f| { current_freq += f; current_freq })
-        .filter_map(|f| if ! hm.insert(f.clone()) { Some(f.clone()) } else { None })
+        .filter(|f| ! hm.insert(f.clone()))
         .next()
         .unwrap();
 
-    // for i in stream {
-    //     println!("{}", i);
-    // }
-
-    println!("{}", p2);
     aoc.submit_p2(p2);
 }
