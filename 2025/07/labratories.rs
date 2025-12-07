896ba3845d239a8c9d21820f450f589c44b3e7fa
Rahul Butani <rrbutani@users.noreply.github.com>
2025-12-06 21:45:09 -0800

    2025: 07, p2

--- 2025/07/labratories.rs
+++ 2025/07/labratories.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, mem};
+use std::{collections::HashMap, mem};
 
 use aoc::{AdventOfCode, Grid, SmallVec};
 use smallvec::smallvec;
@@ -14,32 +14,30 @@ enum Cell {
     Splitter,
 }
 
-// given a list of incoming columns and a row, what outgoing columns will the beam be on?
-fn step_simultaneous<'i, 'r, F: FnMut(usize)>(
-    incoming_cols: &'i HashSet<usize>,
+// given a list of incoming columns (w/counts) and a row, what outgoing columns will the beam be on?
+fn step<'r, I: Iterator<Item = (usize, usize)>, F: FnMut(usize, usize)>(
+    incoming_cols: I, // col, count
     row: &'r [Cell],
     mut split_callback: F,
-) -> impl Iterator<Item = usize> + use<'i, 'r, F> {
-    incoming_cols
-        .iter()
-        .flat_map(move |&c| -> SmallVec<[usize; 2]> {
-            use Cell::*;
-            match row[c] {
-                Start => unreachable!(),
-                Empty => smallvec![c], // pass through
-                Splitter => {
-                    split_callback(c);
-                    let mut out = smallvec![];
-                    if let Some(left) = c.checked_sub(1) {
-                        out.push(left);
-                    }
-                    if let Some(right) = c.checked_add(1).filter(|&c| c < row.len()) {
-                        out.push(right);
-                    }
-                    out
+) -> impl Iterator<Item = (usize, usize)> + use<'r, I, F> {
+    incoming_cols.flat_map(move |(c, count)| -> SmallVec<[(usize, usize); 2]> {
+        use Cell::*;
+        match row[c] {
+            Start => unreachable!(),
+            Empty => smallvec![(c, count)], // pass through
+            Splitter => {
+                split_callback(c, count);
+                let mut out = smallvec![];
+                if let Some(left) = c.checked_sub(1) {
+                    out.push((left, count));
                 }
+                if let Some(right) = c.checked_add(1).filter(|&c| c < row.len()) {
+                    out.push((right, count));
+                }
+                out
             }
-        })
+        }
+    })
 }
 
 fn main() {
@@ -48,19 +46,42 @@ fn main() {
     let grid: Grid<Cell> = aoc.get_input().parse().unwrap();
     let start = grid.find(|&c| c == Cell::Start).next().unwrap();
 
-    let splits = {
-        let (mut curr_cols, mut next_cols) = (HashSet::from([start.col]), HashSet::new());
-        let mut split_count = 0;
-        for row in &(*grid)[start.row + 1..] {
-            next_cols.clear();
-            let new = step_simultaneous(&curr_cols, row, |_| {
-                split_count += 1;
-            });
-            next_cols.extend(new);
-            mem::swap(&mut curr_cols, &mut next_cols);
+    let (mut curr_cols, mut next_cols) = (HashMap::from([(start.col, 1)]), HashMap::new());
+    let mut split_count = 0;
+    for row in &(*grid)[start.row + 1..] {
+        for (col, count) in step(curr_cols.drain(), row, |_, _| {
+            split_count += 1;
+        }) {
+            *next_cols.entry(col).or_default() += count;
         }
+        mem::swap(&mut curr_cols, &mut next_cols);
+    }
 
-        split_count
-    };
-    _ = aoc.submit_p1(splits);
+    _ = aoc.submit_p1(split_count);
+    _ = aoc.submit_p2(curr_cols.values().sum::<usize>());
 }
+
+/*
+
+```
+.......S.......
+.......1.......
+......1^1......
+......1.1......
+.....1^2^1.....
+.....1.2.1.....
+....1^3^3^1....
+....1.3.3.1....
+...1^4^331^1...
+...1.4.331.1...
+..1^5^434^2^1..
+..1.5.434.2.1..
+.1^154^74.21^1.
+.1.154.74.21.1.
+1^2^A^B^B^211^1
+1.2.A.B.B.211.1
+```
+
+`1 + 2 + 10 + 11 + 11 + 2 + 1 + 1 + 1 -> 40`
+
+*/
